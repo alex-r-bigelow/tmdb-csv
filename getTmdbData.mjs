c01a3ff68c12136b6c8e62a34221574450f6ae81
Alex Bigelow <alex-r-bigelow@users.noreply.github.com>
2023-07-27 08:26:23 -0700

    Update script to pull out nested objects as separate tables; add rate limiting

--- getTmdbData.mjs
+++ getTmdbData.mjs
@@ -25,103 +25,225 @@ parser.add_argument('-p', '--pages', {
 
 const args = parser.parse_args();
 
+const FIXED_PROMOTABLE_KEYS = ['id', 'iso_3166_1', 'iso_639_1'];
+
+let currentFetch = Promise.resolve();
+const rateLimitedFetch = async (url) => {
+  currentFetch = currentFetch.then(
+    () =>
+      new Promise((resolve) => setTimeout(() => fetch(url).then(resolve), 100))
+  );
+  return currentFetch;
+};
+
 const getMovieByTopRatedPage = async (pageNo) =>
-  fetch(
+  rateLimitedFetch(
     `https://api.themoviedb.org/3/movie/top_rated?api_key=${args.key}&language=en-US&page=${pageNo}`
   ).then((result) => result.json());
 
 const getMovieDetails = async (movieId) =>
-  fetch(
+  rateLimitedFetch(
     `https://api.themoviedb.org/3/movie/${movieId}?api_key=${args.key}&language=en-US`
   ).then((result) => result.json());
 
 const getMovieCredits = async (movieId) =>
-  fetch(
+  rateLimitedFetch(
     `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${args.key}&language=en-US`
   ).then((result) => result.json());
 
 const getPersonDetails = async (personId) =>
-  fetch(
+  rateLimitedFetch(
     `https://api.themoviedb.org/3/person/${personId}?api_key=${args.key}&language=en-US`
   ).then((result) => result.json());
 
 const run = async () => {
-  const movieHeaders = { all: new Set(), order: [] };
-  const moviesFile = fs.openSync(path.join(args.csv, 'movies.csv'), 'w+');
+  const moviesPath = path.join(args.csv, 'movies.csv');
+  const castPath = path.join(args.csv, 'cast.csv');
+  const crewPath = path.join(args.csv, 'crew.csv');
+  const peoplePath = path.join(args.csv, 'people.csv');
 
-  const castHeaders = { all: new Set(), order: [] };
-  const castFile = fs.openSync(path.join(args.csv, 'cast.csv'), 'w+');
-  const crewHeaders = { all: new Set(), order: [] };
-  const crewFile = fs.openSync(path.join(args.csv, 'crew.csv'), 'w+');
+  const baseFiles = {
+    movies: {
+      objKey: 'id',
+      includedKeys: new Set(),
+      headers: { all: new Set(), order: [] },
+      path: moviesPath,
+      file: fs.openSync(moviesPath, 'w+'),
+    },
+    cast: {
+      objKey: 'credit_id',
+      includedKeys: new Set(),
+      headers: { all: new Set(), order: [] },
+      path: castPath,
+      file: fs.openSync(castPath, 'w+'),
+    },
+    crew: {
+      objKey: 'credit_id',
+      includedKeys: new Set(),
+      headers: { all: new Set(), order: [] },
+      path: crewPath,
+      file: fs.openSync(crewPath, 'w+'),
+    },
+    people: {
+      objKey: 'id',
+      includedKeys: new Set(),
+      headers: { all: new Set(), order: [] },
+      path: peoplePath,
+      file: fs.openSync(peoplePath, 'w+'),
+    },
+  };
+  const junctionFiles = {};
 
-  const peopleHeaders = { all: new Set(), order: [] };
-  const peopleFile = fs.openSync(path.join(args.csv, 'people.csv'), 'w+');
-  const queriedPersonIds = new Set();
-
-  const checkHeaders = (data, headers, outputFile) => {
-    if (headers.all.size === 0) {
-      headers.order = Object.keys(data).filter(
-        (key) => typeof data[key] !== 'object'
-      );
-      headers.all = new Set(headers.order);
+  const checkHeaders = (data, baseFile) => {
+    if (baseFile.headers.all.size === 0) {
+      const sourceKey = path.basename(baseFile.path, '.csv');
+      baseFile.headers.order = Object.keys(data).filter((key) => {
+        const isObject = typeof data[key] === 'object' && data[key] !== null;
+        if (isObject && Object.keys(data[key]).length > 0) {
+          const promotedKey = key;
+          const junctionKey = `${sourceKey}_${promotedKey}`;
+          const isArray = data[promotedKey] instanceof Array;
+          const nestedSample = isArray
+            ? data[promotedKey][0]
+            : data[promotedKey];
+          const promotedObjKey = FIXED_PROMOTABLE_KEYS.find(
+            (pKey) => nestedSample[pKey] !== undefined
+          );
+          if (promotedObjKey === undefined) {
+            console.log(
+              `Skipping nested object (couldn't find a known key for promotion): ${JSON.stringify(
+                nestedSample,
+                null,
+                2
+              )}`
+            );
+            return false;
+          }
+          if (!baseFiles[promotedKey]) {
+            const promotedFilePath = path.join(args.csv, `${promotedKey}.csv`);
+            console.log(
+              `Promoting nested objects as ${promotedFilePath} that look like: ${JSON.stringify(
+                nestedSample,
+                null,
+                2
+              )}`
+            );
+            baseFiles[promotedKey] = {
+              objKey: promotedObjKey,
+              includedKeys: new Set(),
+              headers: { all: new Set(), order: [] },
+              path: promotedFilePath,
+              file: fs.openSync(promotedFilePath, 'w+'),
+            };
+            const junctionFilePath = path.join(args.csv, `${junctionKey}.csv`);
+            junctionFiles[junctionKey] = {
+              headers: { all: new Set(), order: [] },
+              path: junctionFilePath,
+              file: fs.openSync(junctionFilePath, 'w+'),
+            };
+          }
+          checkHeaders(nestedSample, baseFiles[promotedKey]);
+          checkHeaders(
+            { [sourceKey]: '', [promotedKey]: '' },
+            junctionFiles[junctionKey]
+          );
+        }
+        return !isObject;
+      });
+      baseFile.headers.all = new Set(baseFile.headers.order);
       const headerString =
-        Papa.unparse([headers.order], {
-          columns: headers.order,
+        Papa.unparse([baseFile.headers.order], {
+          columns: baseFile.headers.order,
           header: true,
         }) + '\n';
-      fs.writeSync(outputFile, headerString);
+      fs.writeSync(baseFile.file, headerString);
+    } else {
+      // TODO: check and warn if we find additional headers we weren't expecting
     }
   };
 
-  const writeLine = (data, headers, outputFile) => {
+  const writeLine = (data, baseFile) => {
+    if (baseFile.includedKeys) {
+      if (baseFile.includedKeys.has(data[baseFile.objKey])) {
+        return;
+      }
+      baseFile.includedKeys.add(data[baseFile.objKey]);
+    }
     const outputString =
       Papa.unparse([data], {
-        columns: headers.order,
+        columns: baseFile.headers.order,
         header: false,
         newline: '\n',
       }) + '\n';
-    fs.writeSync(outputFile, outputString);
+    fs.writeSync(baseFile.file, outputString);
+    const sourceKey = path.basename(baseFile.path, '.csv');
+    Object.keys(baseFiles).forEach((promotedKey) => {
+      if (
+        promotedKey in data &&
+        typeof data[promotedKey] === 'object' &&
+        data[promotedKey] !== null
+      ) {
+        const lines =
+          data[promotedKey] instanceof Array
+            ? data[promotedKey]
+            : [data[promotedKey]];
+        lines.forEach((line) => {
+          writeLine(line, baseFiles[promotedKey]);
+          const junctionKey = `${sourceKey}_${promotedKey}`;
+          writeLine(
+            {
+              [sourceKey]: data[baseFile.objKey],
+              [promotedKey]: line[baseFiles[promotedKey].objKey],
+            },
+            junctionFiles[junctionKey]
+          );
+        });
+      }
+    });
   };
 
   const queryPerson = async (credit) => {
-    if (!queriedPersonIds.has(credit.id)) {
-      console.log(`Loading details for person: ${credit.name}`);
+    if (!baseFiles.people.includedKeys.has(credit.id)) {
+      console.log(`Querying person: ${credit.name}`);
       const person = await getPersonDetails(credit.id);
-      checkHeaders(person, peopleHeaders, peopleFile);
-      writeLine(person, peopleHeaders, peopleFile);
+      checkHeaders(person, baseFiles.people);
+      writeLine(person, baseFiles.people);
     }
   };
 
-  for (let pageNo = 1; pageNo < parseInt(args.pages); pageNo++) {
+  for (let pageNo = 1; pageNo <= parseInt(args.pages); pageNo++) {
     const movies = await getMovieByTopRatedPage(pageNo);
+    console.log(
+      `Processing ${movies.results.length}${
+        pageNo > 1 ? ' more' : ''
+      } movies...`
+    );
 
     for await (const movie of movies.results) {
-      console.log(`Loading details for movie: ${movie.title}`);
+      console.log(`Querying movie: ${movie.title}`);
       const movieDetails = await getMovieDetails(movie.id);
-      checkHeaders(movieDetails, movieHeaders, moviesFile);
-      writeLine(movieDetails, movieHeaders, moviesFile);
+      checkHeaders(movieDetails, baseFiles.movies);
+      writeLine(movieDetails, baseFiles.movies);
 
-      console.log(`Loading credits for movie: ${movie.title}`);
+      console.log(`Querying credits for movie: ${movie.title}`);
       const { cast, crew } = await getMovieCredits(movie.id);
 
       for await (const rawCredit of cast) {
         const credit = { ...rawCredit, movieId: movie.id };
-        checkHeaders(credit, castHeaders, castFile);
-        writeLine(credit, castHeaders, castFile);
-        queryPerson(credit);
+        checkHeaders(credit, baseFiles.cast);
+        writeLine(credit, baseFiles.cast);
+        await queryPerson(credit);
       }
       for await (const rawCredit of crew) {
         const credit = { ...rawCredit, movieId: movie.id };
-        checkHeaders(credit, crewHeaders, crewFile);
-        writeLine(credit, crewHeaders, crewFile);
-        queryPerson(credit);
+        checkHeaders(credit, baseFiles.crew);
+        writeLine(credit, baseFiles.crew);
+        await queryPerson(credit);
       }
     }
   }
 
-  fs.close(moviesFile);
-  fs.close(castFile);
-  fs.close(crewFile);
-  fs.close(peopleFile);
+  Object.values(baseFiles).forEach(({ file }) => fs.close(file));
+  Object.values(junctionFiles).forEach(({ file }) => fs.close(file));
 };
 run();
